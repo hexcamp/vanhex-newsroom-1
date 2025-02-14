65e6db6716dbc89f1814437d75dca097b137e42a
Mary <git@mary.my.id>
2025-02-14 17:51:49 +0700

    feat: generic error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+	import { page } from '$app/state';
+
+	import PageContainer from '$lib/components/page/page-container.svelte';
+
+	import '$lib/styles/app.css';
+</script>
+
+<PageContainer>
+	<div class="error-page">
+		<h1 class="title">Error {page.status}</h1>
+		<p class="description">
+			{page.error?.message}
+		</p>
+	</div>
+</PageContainer>
+
+<style>
+	.error-page {
+		display: flex;
+		flex-direction: column;
+		gap: 16px;
+		background: var(--bg-primary);
+		padding: 24px;
+	}
+
+	.title {
+		font-size: 1.25rem;
+		line-height: 1.75rem;
+	}
+	.description {
+		font-size: 1rem;
+		line-height: 1.5rem;
+	}
+</style>

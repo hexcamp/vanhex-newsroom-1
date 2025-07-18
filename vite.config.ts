607a0149cfa6fdaffa92dee9b87cbcdf24c8a41a
Mary <git@mary.my.id>
2025-07-18 07:41:22 +0700

    chore: remove payload reduction hack

--- vite.config.ts
+++ vite.config.ts
@@ -13,24 +13,24 @@ export default defineConfig(({ command }) => {
 			sveltekit(),
 
 			// Nasty hack to remove the hydration markers that SvelteKit adds to the HTML
-			command === 'build' && {
-				name: 'remove-hydration-markers',
-				transform(code, id, options) {
-					if (id.endsWith('.svelte') && code.includes('$$payload')) {
-						code = code
-							.replace(/<!--.*?-->/g, '')
-							.replace(/\$\$slots: {.+?},?/g, '')
-							.replace(/\$\$payload\.out \+= ["'`]{2};|\$\.(push|pop)\(\);/g, '')
-							.replace(/(?<=\$\$payload\.out \+= )`\${([a-zA-Z0-9_$.,()[\]\s]+?)}`(?=;)/, '$1');
+			// command === 'build' && {
+			// 	name: 'remove-hydration-markers',
+			// 	transform(code, id, options) {
+			// 		if (id.endsWith('.svelte') && code.includes('$$payload')) {
+			// 			code = code
+			// 				.replace(/<!--.*?-->/g, '')
+			// 				.replace(/\$\$slots: {.+?},?/g, '')
+			// 				.replace(/\$\$payload\.out \+= ["'`]{2};|\$\.(push|pop)\(\);/g, '')
+			// 				.replace(/(?<=\$\$payload\.out \+= )`\${([a-zA-Z0-9_$.,()[\]\s]+?)}`(?=;)/, '$1');
 
-						return code;
-					} else if (id.includes('node_modules/svelte/') && code.includes('<!--')) {
-						code = code.replace(/(['`])<!--.*?-->\1/g, '$1$1');
+			// 			return code;
+			// 		} else if (id.includes('node_modules/svelte/') && code.includes('<!--')) {
+			// 			code = code.replace(/(['`])<!--.*?-->\1/g, '$1$1');
 
-						return code;
-					}
-				},
-			},
+			// 			return code;
+			// 		}
+			// 	},
+			// },
 		],
 	};
 });

f8558a8ca187991ae4b1d4baff98496975328d99
Jim Pick <jim@jimpick.com>
2026-05-11 12:14:18 -0700

    s/newsroom/newshub/cg

--- svelte.config.js
+++ svelte.config.js
@@ -18,7 +18,7 @@ const config = {
 			// See: https://svelte.dev/docs/kit/adapter-static
 			// default options are shown. On some platforms
 			// these options are set automatically — see below
-			pages: 'site/6kg6rsiaaaaa-vanhex-newsroom-1',
+			pages: 'site/6kg6rsiaaaaa-vanhex-newshub-1',
 			// assets: 'build',
 			fallback: 'index.html',
 			precompress: false,
